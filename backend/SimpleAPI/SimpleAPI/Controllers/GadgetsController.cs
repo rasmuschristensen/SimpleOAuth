16d0d9266c7b0ed3e752f68dde6bafa524f23003
Rasmus Christensen <rasmus@bypassion.dk>
2016-01-12 00:41:11 +0100

    added gadgets controller

---
+++ backend/SimpleAPI/SimpleAPI/Controllers/GadgetsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SimpleAPI.Controllers
+{
+    [Authorize]
+    public class GadgetsController : ApiController
+    {
+        public List<GadgetItem> Get()
+        {
+            return new List<GadgetItem>
+            {
+                new GadgetItem(1, "MBP"),
+                new GadgetItem(2, "IPad mini"),
+                new GadgetItem(3, "Nexus 5x"),
+                new GadgetItem(4, "Tesla Model X")
+            };
+        }
+    }
+
+    public class GadgetItem
+    {
+        public GadgetItem(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
